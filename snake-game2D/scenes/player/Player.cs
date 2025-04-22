bb79f677723bb80f0c18c3f2a270b3d3a4a413e4
OverLuked <edillorluke@gmail.com>
2025-04-22 23:35:06 +0800

    Signal implementation to player

--- snake-game2D/scenes/player/Player.cs
+++ snake-game2D/scenes/player/Player.cs
@@ -1,13 +1,19 @@
 using Godot;
-using System;
+
 
 public partial class Player : CharacterBody2D
 {
     [Export] public float Speed;
     [Export] public bool IsAlive;
+    public bool OnScreen;
 
-    public bool IsOnScreen;
-
+    private ISignalService _signalService;
+    
+    // Inject signal service
+    public void Inject(ISignalService signalService)
+    {
+        _signalService = signalService;
+    }
     public override void _Ready()
     {
         GD.Print("Player Node Ready!");
@@ -17,5 +23,17 @@ public partial class Player : CharacterBody2D
     private void OnExit()
     {
         GD.Print("Player Exited Screen");
+        OnScreen = true;
+    }
+    
+    // Signal Player Death
+    private void OnPlayerDeath()
+    {
+        GD.Print("Player Dead! Game Over!");
+    }
+
+    private void ConnectSignals()
+    {
+        _signalService?.ConnectSignal("PlayerDeath", this, nameof(OnPlayerDeath));
     }
 }
