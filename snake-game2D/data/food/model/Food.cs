4cd75f93fe6da9833d758f40b0e1ab51935bca62
OverLuked <edillorluke@gmail.com>
2025-04-23 23:19:46 +0800

    Add food resource

---
+++ snake-game2D/data/food/model/Food.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Food : Resource
+{
+    
+    [Export] public string Name;
+    [Export] public int Point;
+    [Export] public Texture Texture;
+}
