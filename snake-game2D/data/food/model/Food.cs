b35c1e5e607d6edde465f279f6b27e8043e07860
OverLuked <edillorluke@gmail.com>
2025-04-23 23:46:54 +0800

    Implement food resource in FoodInstaance

--- snake-game2D/data/food/model/Food.cs
+++ snake-game2D/data/food/model/Food.cs
@@ -7,5 +7,5 @@ public partial class Food : Resource
     
     [Export] public string Name;
     [Export] public int Point;
-    [Export] public Texture Texture;
+    [Export] public Texture2D Texture;
 }
