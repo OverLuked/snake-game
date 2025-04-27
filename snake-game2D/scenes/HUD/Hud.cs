19fc8487f3b51161c9c9daa451ed580c1df2e572
OverLuked <edillorluke@gmail.com>
2025-04-27 12:50:55 +0800

    Add update score

--- snake-game2D/scenes/HUD/Hud.cs
+++ snake-game2D/scenes/HUD/Hud.cs
@@ -1,12 +1,24 @@
 using Godot;
 using System;
+using System.Net.Mime;
 
 public partial class Hud : Control
 {
     [Export] private Label _label;
 
+    private int _score = 0;
+
     public override void _Ready()
     {
         GD.Print("Hud: Ready!");
+        GetNode<SignalBus>("/root/SignalBus").OnEaten += UpdateScore;
+        
+        _label.SetText(_score.ToString());
+    }
+
+    private void UpdateScore(GodotObject food)
+    {
+        _score += 1;
+        _label.SetText(_score.ToString());
     }
 }
