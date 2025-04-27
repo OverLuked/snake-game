855ceb66fdb956de741900cfafe1d3ea64d8f0d9
OverLuked <edillorluke@gmail.com>
2025-04-27 12:29:05 +0800

    Signal flow rewrite

--- snake-game2D/core/signal/ISignalService.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-public interface ISignalService
-{
-    void ConnectSignal(string signalName, GodotObject target, string methodName);
-    void EmitSignal(string signalName, params GodotObject[] args);
-    void DisconnectSignal(string signalName, GodotObject target, string methodName);
-}
